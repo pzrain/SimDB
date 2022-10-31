a2dfedb0cdde4ce2f55ef7eda1aa48e03a5a23ae
潘子睿 <maspzr@163.com>
2022-10-31 23:43:39 +0800

    implement IndexPage(without test)

--- src/index/IndexPage.cpp
+++ src/index/IndexPage.cpp
@@ -3,17 +3,18 @@
 void IndexPageHeader::initialize(uint16_t indexLen_, uint8_t colType_) {
     firstIndex = -1;
     firstEmptyIndex = -1;
+    nextPage = -1;
+    lastPage = -1;
     totalIndex = 0;
     childIndex = -1;
     indexLen = indexLen_;
     isInitialized = 1;
-    pageType = PAGE_LEAF;
     colType = colType_;
 }
 
 inline uint16_t IndexPageHeader::getTotalLen() {
-    // nextIndex + content + val
-    return indexLen + sizeof(int16_t) + sizeof(int);
+    // nextIndex + lastIndex + childIndex + content + val
+    return sizeof(int16_t) + sizeof(int16_t) + sizeof(int16_t) + indexLen + sizeof(int);
 }
 
 IndexPage::IndexPage(uint8_t* pageData, uint16_t indexLen, uint8_t colType) {
@@ -22,5 +23,179 @@ IndexPage::IndexPage(uint8_t* pageData, uint16_t indexLen, uint8_t colType) {
     if (indexPageHeader->isInitialized == 0) { // uninitialized but not zero?
         indexPageHeader->initialize(indexLen, colType);
     }
-    capacity = (PAGE_SIZE - sizeof(IndexPageHeader)) / indexPageHeader->getTotalLen();
+    capacity = ((PAGE_SIZE - sizeof(IndexPageHeader)) / indexPageHeader->getTotalLen()) - 1;
+    switch (colType) {
+        case COL_INT:
+            compare = new IntCompare();
+            break;
+        case COL_FLOAT:
+            compare = new FloatCompare();
+            break;
+        case COL_VARCHAR:
+            compare = new CharCompare();
+            break;
+        default:
+            compare = nullptr;
+            break;
+    }
+}
+
+IndexPage::~IndexPage() {
+    if (compare) {
+        delete compare;
+    }
+}
+
+uint16_t IndexPage::getCapacity() {
+    return capacity;
+}
+
+bool IndexPage::overflow() {
+    return indexPageHeader->totalIndex > capacity;
+}
+
+bool IndexPage::underflow() {
+    return indexPageHeader->totalIndex < (capacity + 1) / 2;
+}
+
+uint8_t* IndexPage::accessData(int id) {
+    if (id < 0 || id > capacity) {
+        // id == capacity will result in overflow
+        printf("[ERROR] fetch slot with wrong id.\n");
+        return nullptr;
+    }
+    uint16_t totalLen = indexPageHeader->getTotalLen();
+    return data + sizeof(indexPageHeader) + id * totalLen;
+}
+
+void* IndexPage::getData(int id) {
+    uint8_t* data = accessData(id);
+    return data + 3 * sizeof(int16_t);
+}
+
+int IndexPage::getVal(int id) {
+    uint8_t* data = accessData(id);
+    return *((int*)(data + 3 * sizeof(int16_t) + indexPageHeader->indexLen));
+}
+
+int16_t IndexPage::getNextIndex(int id) {
+    uint8_t* data = accessData(id);
+    return *((int16_t*)data);
+}
+
+int16_t IndexPage::getLastIndex(int id) {
+    uint8_t* data = accessData(id);
+    return *((int16_t*)(data + sizeof(int16_t)));
+}
+
+int16_t IndexPage::getChildIndex(int id) {
+    uint8_t* data = accessData(id);
+    return *((int16_t*)(data + 2 * sizeof(int16_t)));
+}
+
+int IndexPage::insert(void* data, const int val, const int16_t childIndex_) {
+    int16_t head = indexPageHeader->firstIndex, last = -1;
+    int16_t emptyIndex = indexPageHeader->firstEmptyIndex;
+    indexPageHeader->totalIndex++;
+    if (emptyIndex < 0) {
+        // new empty slot, create and initialize
+        emptyIndex = indexPageHeader->totalIndex;
+        uint8_t* newEmptySlot = accessData(emptyIndex);
+        ((int16_t*)newEmptySlot)[0] = -1;
+    }
+    uint8_t* emptySlot = accessData(emptyIndex);
+    indexPageHeader->firstEmptyIndex = ((int16_t*)emptySlot)[0];
+
+    if (head < 0) {
+        indexPageHeader->firstIndex = emptyIndex;
+    } else {
+        while (head >= 0) {
+            if (compare->lte(data, getData(head))) {
+                break;
+            }
+            last = head;
+            head = getNextIndex(head);
+        }
+    }
+
+    int16_t* nextIndex = (int16_t*)emptySlot;
+    int16_t* lastIndex = ((int16_t*)emptySlot) + 1;
+    int16_t* childIndex = ((int16_t*)emptySlot) + 2;
+    void* emptyData = emptySlot + 3 * sizeof(int16_t);
+    int* emptyVal = (int*)(emptySlot + 3 * sizeof(int16_t) + indexPageHeader->indexLen);
+    *nextIndex = head;
+    *lastIndex = last;
+    if (last >= 0) {
+        uint8_t* lastSlot = accessData(last);
+        ((int16_t*)lastSlot)[0] = emptyIndex;
+    }
+    if (head >= 0) {
+        uint8_t* headSlot = accessData(head);
+        ((int16_t*)headSlot)[1] = emptyIndex;
+    }
+    *childIndex = childIndex_;
+    *emptyVal = val;
+    memcpy(emptyData, data, indexPageHeader->indexLen);
+    return emptyIndex;
+}
+
+void IndexPage::searchEQ(void* data, std::vector<int> &res) {
+    int16_t head = indexPageHeader->firstIndex;
+    while (head >= 0) {
+        if (compare->equ(data, getData(head))) {
+            res.push_back(head);
+        } else if (compare->lt(data, getData(head))) {
+            break;
+        }
+    }
+}
+
+int IndexPage::searchLowerBound(void* data) {
+    int16_t head = indexPageHeader->firstIndex, last = -1;
+    while (head >= 0) {
+        if (compare->lt(data, getData(head))) {
+            break;
+        }
+        last = head;
+        head = getNextIndex(head);
+    }
+    return last;
+}
+
+int IndexPage::searchUpperBound(void* data) {
+    int16_t head = indexPageHeader->firstIndex;
+    while (head >= 0) {
+        if (compare->lte(data, getData(head))) {
+            break;
+        }
+    }
+    return head;
+}
+
+void IndexPage::remove(void* data, std::vector<int> &res) {
+    int16_t head = indexPageHeader->firstIndex, last = -1;
+    while (head >= 0) {
+        int next = getNextIndex(head);
+        if (compare->equ(data, getData(head))) {
+            res.push_back(head);
+            if (last >= 0) {
+                uint8_t* lastData = accessData(last);
+                ((int16_t*)lastData)[0] = next;
+            }
+            if (next >= 0) {
+                uint8_t* nextData = accessData(next);
+                ((int16_t*)(nextData + sizeof(int16_t)))[0] = last;
+            }
+            uint8_t* emptyData = accessData(head);
+            // set empty index
+            ((int16_t*)emptyData)[0] = indexPageHeader->firstEmptyIndex;
+            indexPageHeader->firstEmptyIndex = head;
+
+            indexPageHeader->totalIndex--;
+            head = next;
+        } else {
+            last = head;
+            head = next;
+        }
+    }
 }
--- src/index/IndexPage.h
+++ src/index/IndexPage.h
@@ -2,28 +2,21 @@
 #define __INDEX_PAGE_H__
 #include "../filesystem/utils/pagedef.h"
 #include "../common.h"
+#include "compare.h"
 #include <stdint.h>
-
-typedef enum {
-    PAGE_RAW = 0,
-    PAGE_INTERIOR = 1,
-    PAGE_LEAF = 2
-} IndexPageType;
+#include <vector>
 
 struct IndexPageHeader{
     uint8_t isInitialized;
-    uint8_t pageType;
     uint8_t colType;
+    int16_t nextPage; // related to B+ tree
+    int16_t lastPage;
     int16_t firstIndex;
     int16_t firstEmptyIndex;
     uint16_t totalIndex;
     uint16_t childIndex;
     uint16_t indexLen;
-    union {
-        int intContent;
-        float floatContent;
-        char charContent[TAB_MAX_LEN];
-    } maxContent;
+    uint8_t maxContent[TAB_MAX_LEN];
 
     void initialize(uint16_t indexLen_, uint8_t colType_);
 
@@ -31,24 +24,45 @@ struct IndexPageHeader{
 };
 
 class IndexPage{
+private:
+    IndexPageHeader* indexPageHeader;
+    Compare* compare;
+    uint16_t capacity;
 public:
     uint8_t* data;
-    IndexPageHeader* indexPageHeader;
-    uint16_t capacity;
 
     IndexPage(uint8_t* pageData, uint16_t indexLen, uint8_t colType);
 
-    virtual ~IndexPage() = 0;
-};
+    ~IndexPage();
 
-class InteriorPage: public IndexPage{
-public:
-    ~InteriorPage();
-};
+    uint16_t getCapacity();
 
-class LeafPage: public IndexPage{
-public:
-    ~LeafPage();
+    bool overflow();
+
+    bool underflow();
+
+    uint8_t* accessData(int id);
+
+    void* getData(int id);
+
+    int getVal(int id);
+
+    int16_t getNextIndex(int id);
+
+    int16_t getLastIndex(int id);
+
+    int16_t getChildIndex(int id);
+
+    int insert(void* data, const int val, const int16_t childIndex_ = -1); // key and value
+
+    void searchEQ(void* data, std::vector<int> &res);
+
+    int searchLowerBound(void* data); // return the uppermost index of slots whose value is no greater than data
+                                      // -1 if all value are greater than data
+
+    int searchUpperBound(void* data);
+
+    void remove(void* data, std::vector<int> &res);
 };
 
 #endif
