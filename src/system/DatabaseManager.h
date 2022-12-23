53def9e764b91aab41555cc3a5643ddb7aff6712
潘子睿 <maspzr@gmail.com>
2022-12-23 22:38:50 +0800

    add comment and doc on system-dev

--- src/system/DatabaseManager.h
+++ src/system/DatabaseManager.h
@@ -23,8 +23,6 @@ private:
     BufPageManager* bufPageManager;
     TableManager* tableManager;
 
-
-
     inline bool checkDatabaseName(string name);
 
     inline bool checkFileExist(string path);
@@ -102,14 +100,23 @@ public:
     int renameTable(string oldName, string newName);
 
     /**
-     * not sure
-    */
+     * @brief Create a index on a specific table.column
+     * 'ALTER' 'TABLE' Identifier 'ADD' 'INDEX' '(' identifiers ')'   
+     */
     int createIndex(string tableName, string colName);
 
+    /**
+     * @brief Drop the index on table.column
+     * 'ALTER' 'TABLE' Identifier 'DROP' 'INDEX' '(' identifiers ')'
+     */
     int dropIndex(string tableName, string colName);
 
     int hasIndex(string tableName, string colName);
 
+    /**
+     * @brief Display all the index of a table
+     * 'SHOW' 'INDEXES'
+     */
     int showIndex();
 
     /**
@@ -148,18 +155,34 @@ public:
 
     /**
      * @brief select statement, including fuzzy, nesty, group by…… selection
-     * 
+     * 'SELECT' selectors 'FROM' identifiers ('WHERE' where_and_clause)? ('GROUP' 'BY' column)? ('LIMIT' Integer ('OFFSET' Integer)?)?
      * @param dbSelect 
      * @return number of queries affected, -1 if encounters error
      */
     int selectRecords(DBSelect* dbSelect);
-    // SELECT column FROM table WHERE expr;
-    // parameters are encapsulated mainly due to nesty selections
 
+    /**
+     * @brief update statement
+     * 'UPDATE' Identifier 'SET' set_clause 'WHERE' where_and_clause
+     * @param dbUpdate 
+     * @return int 
+     */
     int updateRecords(string tableName, DBUpdate* dbUpdate);
 
+    /**
+     * @brief insert statement
+     * 'INSERT' 'INTO' Identifier 'VALUES' value_lists
+     * @param dbInsert 
+     * @return int 
+     */
     int insertRecords(string tableName, DBInsert* dbInsert);
 
+    /**
+     * @brief delete statement
+     * 'DELETE' 'FROM' Identifier 'WHERE' where_and_clause
+     * @param dbDelete 
+     * @return int 
+     */
     int dropRecords(string tableName, DBDelete* dbDelete);
 
 };
