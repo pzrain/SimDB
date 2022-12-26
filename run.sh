88afa99aee7e874e272f908e77b27fbd1b410593
潘子睿 <maspzr@gmail.com>
2022-12-26 23:48:27 +0800

    support batch mode

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-COMPILE_FLAG=$1
+FLAG_1=$1
+IN_FILE=$2
+OUT_FILE=$3
+FLAG_4=$4
 
-if [ $# -eq 1 ] && [ $COMPILE_FLAG = '-c' ]; then
+if ([ $# -eq 1 ] && [ $FLAG_1 = '-c' ]) || ([ $# -eq 4 ] && [ $FLAG_4 = '-c' ]); then
     if [[ ! -d build ]]; then
         echo "good"
     else
@@ -14,7 +17,13 @@ if [ $# -eq 1 ] && [ $COMPILE_FLAG = '-c' ]; then
 
     echo "Done!"
 fi
-rm -r database
+
 mkdir -p database
-./bin/SimDB 2>msg.debug 1>result.out
-# rm -r database
+
+if [ $# -ge 1 ] && [ $FLAG_1 = '-batch' ]; then
+    echo "batch mode"
+    ./bin/SimDB 2>msg.debug 1>$OUT_FILE $IN_FILE
+else
+    echo "cli mode"
+    ./bin/SimDB 2>/dev/null
+fi
