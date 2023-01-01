ade95a78a583714eaa23cb2357946b8678320ae9
潘子睿 <maspzr@gmail.com>
2023-01-01 21:01:48 +0800

    add testIndex

--- testcases/gen.py
+++ testcases/gen.py
@@ -1,15 +1,16 @@
 import argparse
 ARG = argparse.ArgumentParser()
 ARG.add_argument("--file", type=str, required=True)
+ARG.add_argument("--num", type=int, default=100000)
 ARG = ARG.parse_args()
 
 id = 0
 with open(ARG.file, 'w') as f:
-    f.write("CREATE DATABASE testMoreIndex;\n")
-    f.write("USE testMoreIndex;\n")
+    f.write("CREATE DATABASE testIndex;\n")
+    f.write("USE testIndex;\n")
     f.write("CREATE TABLE test (id INT, cid INT);\n")
     com = "INSERT INTO test VALUES"
-    for id in range(1000):
+    for id in range(ARG.num):
         com += " (%d, %d)," % (id, id)
     com = com[:-1]
     com += ";\n"
