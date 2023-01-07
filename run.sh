2cdb8efe1181bd193d59af8493689333fbf59730
潘子睿 <maspzr@gmail.com>
2023-01-07 17:18:39 +0800

    direct stderr to null

--- run.sh
+++ run.sh
@@ -22,10 +22,10 @@ mkdir -p database
 
 if [ $# -ge 1 ] && [ $FLAG_1 = '-batch' ]; then
     echo "batch mode"
-    ./bin/SimDB 2>msg.debug 1>$OUT_FILE $IN_FILE
+    ./bin/SimDB 2>/dev/null 1>$OUT_FILE $IN_FILE
 elif [ $# -ge 1 ] && [ $FLAG_1 = '-onlyc' ]; then
     echo "only compile"
 else
     echo "cli mode"
-    ./bin/SimDB 2>msg.debug
+    ./bin/SimDB 2>/dev/null
 fi
